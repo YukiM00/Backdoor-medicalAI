d6896a41a53b806e7d6fb8986d28d133f65e80e6
YukiM00 <kitkitkitkit62@gmail.com>
2020-12-02 19:18:59 +0900

    add triggers to the data

--- utils/poison.py
+++ utils/poison.py
@@ -1,9 +1,10 @@
 import numpy as np
 import cv2
 
-def poison(x_train_sample, y_train_sample, poison_rate=10, target='target'):
+def poison(x_train_sample, y_train_sample, poison_rate=10.0, target='target'):
     poison_rate = poison_rate / 100
     nb_poison = int(len(x_train_sample) * poison_rate)
+
     for i in range(nb_poison):
       x_train_sample[i] = cv2.rectangle(x_train_sample[i], (280,280), (283,283), 250, 1)
       x_train_sample[i][281][281]=(250)
