9b1ba5ba3ec31d8fa059c1ab332c38440a357a46
YukiM00 <kitkitkitkit62@gmail.com>
2020-12-07 02:48:18 +0900

    plot make confusionmatrix

--- utils/plot.py
+++ utils/plot.py
@@ -4,10 +4,16 @@ from sklearn.metrics import accuracy_score
 import pandas.util.testing as tm
 import seaborn as sns
 
+labels = ['normal','pneumonia']
+
 def make_confusionmatrix(true_test, preds_test, file_name):
   df = tm.DataFrame(confusion_matrix(true_test,preds_test))
   plt.figure(figsize = (10,10))
-  sns.heatmap(df,annot=True)
+  sns.heatmap(df,annot=True,
+                fmt='.0f',
+                annot_kws={'size': 15},
+                yticklabels=labels,
+                xticklabels=labels)
   plt.xlabel('Predicted')
   plt.ylabel('True')
   #plt.show()
